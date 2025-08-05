543f38699fa3372c00249fc8ebd9627b03ae2f71
ROMA96x <marco.bellusci@icloud.com>
2025-08-05 19:56:35 +0200

    first commit

--- target/debug/examples/read-c6f83a1ad919782c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\bellu\Documents\rust_scripts\can_tools\target\debug\examples\libread-c6f83a1ad919782c.rmeta: examples\read.rs
-
-C:\Users\bellu\Documents\rust_scripts\can_tools\target\debug\examples\read-c6f83a1ad919782c.d: examples\read.rs
-
-examples\read.rs:
--- target/debug/examples/read.d
+++
@@ -1,5 +0,0 @@
-C:\Users\bellu\Documents\rust_scripts\can_tools\target\debug\examples\read.exe: examples\read.rs
-
-C:\Users\bellu\Documents\rust_scripts\can_tools\target\debug\examples\read.d: examples\read.rs
-
-examples\read.rs:
